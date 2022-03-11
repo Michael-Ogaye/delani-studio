b13cda361109c27d43f599b0acfec84c3e6e4587
Michael Ogaye <hogaeymic@gmail.com>
2022-03-12 00:15:09 +0300

    bug: images reference not found

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(window).load(function(){
-    
+    let icos= $('#proc img');
+    icos.each(function(){
+        $(this).click(function(){
+            $('Proc div p.card-text').slideToggle(500)
+        })
+    })
+        
+
+
+
+
 
 
 
