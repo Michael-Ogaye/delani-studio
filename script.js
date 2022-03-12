2601cee0b809647a2261d7d1ecb7c2d80afe4dd5
Michael Ogaye <hogaeymic@gmail.com>
2022-03-12 16:00:30 +0300

    feature:the portofolio cards are flipping,aded by jquery effects

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 $(document).ready(function(){
     let icos= $('.card');
     icos.click(function(){
-        // $('.card p.card-text').slideToggle(500);
+    
+ $(this).find('p.card-text').slideToggle(500).closest('.card').find('.card-img-top').slideToggle(500);
 
-        $(this).find('p.card-text').slideToggle(500) 
-        
+   
+
+
+
     })
+    
 
-
-
-
-
-
-
-
-
-
-
+    //flipping the cards
+let flipas=$('.panels li');
+flipas.hover(function(){
+    $(this).find('.top').toggle().closest('li').find('.bot').show();
+})
 
 
 
